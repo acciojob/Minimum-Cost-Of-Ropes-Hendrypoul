b4e1964f1748afa19f1f3b60c6c14c1348d60294
Hendrypoul <106142503+Hendrypoul@users.noreply.github.com>
2023-02-20 08:06:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-function calculateMinCost() {
-  //your code here
-  
-  
-  
-}  
+const inputElemenet = document.getElementTagName("input")[0];
+const inputs = inputElemenet.value;
+const arr = inputs.split(',');
+console.log(arr);
