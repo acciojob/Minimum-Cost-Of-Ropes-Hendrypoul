769283659dedb224aaa817bed94361b6ffd2b2fc
Hendrypoul <106142503+Hendrypoul@users.noreply.github.com>
2023-02-21 01:38:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function minCostToFormRope(a) {
 	a.preventDefault();
 	var inputElement = document.querySelector('input').value;
 	var arr = inputElement.split(',');
-	arr.sort(function (a,b) {return a-b});
+	arr.sort(function (a,b) { return a-b});
 	var cost = 0;
 	while(arr.length > 1) {
 		var res = Number(arr[0]) + Number(arr[1]);
