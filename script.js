0761111a794cad5aa4889f5e111f9e125df71811
Hendrypoul <106142503+Hendrypoul@users.noreply.github.com>
2023-02-21 01:24:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-const inputElemenet = document.getElementTagName("input")[0];
-const inputs = inputElemenet.value;
-const arr = inputs.split(',');
-console.log(arr);
+function minCostToFormRope(event) {
+	event.preventDefault();
+	var inputElement = document.querySelector('input').value;
+	var arr = inputElement.split(',');
+	arr.sort(function (a, b) {return a-b});
+	var cost = 0;
+	while(arr.length > 1) {
+		var res = Number(arr[0]) + Number(arr[1]);
+		arr.push(res);
+		cost += res;
+		arr.shift();
+		arr.shift();
+		arr.sort(function (a, b) { return a-b});
+	}
+	document.getElementById("result").textContent = cost;
+	
+}
